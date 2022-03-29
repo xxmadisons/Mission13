cfa9c61024f5453caaa6a384ca4081ad8178d4ed
xxmadisons <Jazz5089@hotmail.com>
2022-03-29 14:18:27 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController()
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mission13/Models/BowlersDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class BowlersDbContext : DbContext
+    {
+        private static DbContextOptions options;
+
+        public BowlersDbContext(DbContextOptions<BowlersDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bowler> Bowlers { get; set; }
+
+    }
+}
